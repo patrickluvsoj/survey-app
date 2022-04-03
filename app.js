d04990c0a10fb280b6b9f121f661c1ae6a520383
patrickluvsoj <patricklovesoj@gmail.com>
2022-04-03 16:08:50 -0700

    updated to use envariable port

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+
+//get port from env variable passed by Heroku or use local port
+const port =  process.env.PORT || 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
