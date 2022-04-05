de5852b7e8f3e3c84be75e7f240b803f1974715e
patrickluvsoj <patricklovesoj@gmail.com>
2022-04-04 20:06:27 -0700

    added routes to get users google profile

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const express = require('express')
+const passport = require('passport')
+const cookie_session = require('cookie-session')
+const dev = require('./config/dev')
+
+
 const app = express()
+
+
+//adding cookie & passport middleware
+app.use(cookie_session({
+  name: 'session',
+  keys: [dev.COOKIE_KEY],
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  secret: true
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+require('./routes/authRoutes')(app)
+
 
 //get port from env variable passed by Heroku or use local port
 const port =  process.env.PORT || 3000
@@ -9,7 +30,14 @@ app.get('/', (req, res) => {
 })
 
 
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// DONE - Install necessary packages like passport, google etc 
+// DONE - Setup routes for handling auth
+// DOEN - Add the required middleware to setup auth (i think)
+
+// START HERE
+// Have some kind of function to handle authentication
---
+++ config/dev.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    GOOGLE_CLIENT: '975794480741-qail637e5da5suglvojttekhs43grfl4.apps.googleusercontent.com',
+    GOOGLE_SECRET: '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',
+    COOKIE_KEY: 'justsomerandomstring',
+};
