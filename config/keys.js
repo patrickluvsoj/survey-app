65536a7f20c7766c9b03c9c28ab53798f5a8c31b
patrickluvsoj <patricklovesoj@gmail.com>
2022-04-24 19:31:33 -0700

    removed gitignote

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// setup conditional to use different keys!
+if (process.env.NODE_ENV == 'production') {
+    console.log('setting prod keys')
+    module.exports = require('./prod')
+} else {
+    console.log('setting dev keys')
+    module.exports = require('./dev')
+}
