e0c43d2e5bddcf542894bf99599c6649e0f4fef5
patrickluvsoj <patricklovesoj@gmail.com>
2022-04-24 19:34:02 -0700

    update gitignore

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    GOOGLE_CLIENT: process.env.GOOGLE_CLIENT,
+    GOOGLE_SECRET: process.env.GOOGLE_SECRET,
+    COOKIE_KEY: process.env.COOKIE_KEY,
+    MONGO_URL: process.env.MONGO_URL
+};
