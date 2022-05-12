717577bf065f9697fc61432344eb6034828a02dd
patrickluvsoj <patricklovesoj@gmail.com>
2022-05-11 20:01:28 -0700

    added material css styles

---
+++ client/src/Components/Header.js
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
+import { useRecoilValueLoadable } from "recoil";
+import { fetchLoginState } from "../Services/fetchLoginState";
+
+
+function Header() {
+    const isLoggedIn = useRecoilValueLoadable(fetchLoginState);
+    const { contents } = isLoggedIn
+    
+    useEffect(() => {
+        console.log("This is it: \n" + contents)
+    })
+
+    return (
+        <di>
+            <nav>
+                <div className="nav-wrapper">
+                    <a className="brand-logo" href="/">Home</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li>{contents ? "Logout" : "Login"}</li>
+                    </ul>
+                </div>
+            </nav>
+            <Outlet />
+        </div>
+    );
+  }
+  
+  export default Header;
+  
+  
+  
