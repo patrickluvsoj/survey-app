c896f11734a1edb6d22a36f7f088ad96e753ddf7
patrickluvsoj <patricklovesoj@gmail.com>
2022-09-25 16:25:47 -0700

    Initial parse of webhook events from sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const SurveySchema = mongoose.model('surveys');
 const express = require('express');
+// const bodyParser = require('body-parser');
 const { URL } = require('url');
 const { Path } = require('path-parser')
-const _ = rquire('lodash');
+const _ = require('lodash');
 
 const sgMail = require('@sendgrid/mail');
 const keys = require('../config/keys');
@@ -78,8 +79,35 @@ module.exports = function surveyRoutes(app) {
     });
 
 
-    app.post('/api/surveys/webhooks', (req, res) => {
-        res.send("Received webhook event: ", req.body);
+    app.post('/api/surveys/webhooks', express.json(), (req, res) => {
+        // console.log(`Received webhook event: ${JSON.stringify(req.body)}`);
+        // UPDATE sendgrid URL after running NGROK
+
+        // Set NGROK to receive sendgrid events
+            // Enable sendgrid events
+        // Parse the Sendgrid event to get relevant information
+        const responses = _.chain(req.body).map( ({event, email, url}) => {
+            console.log(event);
+            return event === 'click' ? {email, url} : null
+        })
+
+
+        // Encode survey_id in the URL so you can parse and idenity which survey user responded to
+        // Filter the event coming from Sendgrid
+            // We should return survey_id, email & choice
+                // Remove any ones that are not click events
+                // Remove undefined events
+                // use url, lodash & Parse libraries => use chain function
+        // Save the events in MongoDB
+            // Do all search and update in a query
+                // First find the right user using survey_id, email & choice
+                // then update using $inc, $set
+                // add last responded date lastreponded: new Date()
+        // Make sure thank you route is updated
+        console.log(`Received webhook event: ${JSON.stringify(responses)}`);
+
+
+        res.send(`Received webhook event: ${req.body}`);
     });
 
 
@@ -87,3 +115,19 @@ module.exports = function surveyRoutes(app) {
         res.send("Thank you for submitting a response!");
     });
 }
+
+
+
+
+// SENDGRID EVENT STRUCTURE
+// [{"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"processed","category":["cat facts"],"sg_event_id":"EFlb32iXNb_XmuV29w95PQ==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"deferred","category":["cat facts"],"sg_event_id":"BFq3tAKmWmzYLWRTZU5s3A==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","response":"400 try again later","attempt":"5"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"delivered","category":["cat facts"],"sg_event_id":"vC8wJnv4_2PRpRRY9VcZsg==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","response":"250 OK"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"open","category":["cat facts"],"sg_event_id":"6_LJGllWCDMwzfCIlYHKCA==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","useragent":"Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)","ip":"255.255.255.255"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"click","category":["cat facts"],"sg_event_id":"9g9u5Sk1Hrp-avFieGR2pg==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","useragent":"Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)","ip":"255.255.255.255","url":"http://www.sendgrid.com/"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"bounce","category":["cat facts"],"sg_event_id":"Qn_R5pH5Z4napfBubqGi7Q==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","reason":"500 unknown recipient","status":"5.0.0"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"dropped","category":["cat facts"],"sg_event_id":"X1ADYFP3kM5id94plECz3w==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","reason":"Bounced Address","status":"5.0.0"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"spamreport","category":["cat facts"],"sg_event_id":"WK_OfaHiqnKFvzFy6vGETw==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"unsubscribe","category":["cat facts"],"sg_event_id":"cFDjKZsZIHT2m4aO8IUW3g==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0"},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"group_unsubscribe","category":["cat facts"],"sg_event_id":"pN-oCBlWxYVTxTGdLDtzgA==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","useragent":"Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)","ip":"255.255.255.255","url":"http://www.sendgrid.com/","asm_group_id":10},
+// {"email":"example@test.com","timestamp":1664123380,"smtp-id":"\u003c14c5d75ce93.dfd.64b469@ismtpd-555\u003e","event":"group_resubscribe","category":["cat facts"],"sg_event_id":"Btihg1bdIDlM1oVGexvRLw==","sg_message_id":"14c5d75ce93.dfd.64b469.filter0001.16648.5515E0B88.0","useragent":"Mozilla/4.0 (compatible; MSIE 6.1; Windows XP; .NET CLR 1.1.4322; .NET CLR 2.0.50727)","ip":"255.255.255.255","url":"http://www.sendgrid.com/","asm_group_id":10}]
