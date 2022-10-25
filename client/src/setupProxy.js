eab06d08a14baba2d8ad2dae5caa252bcb4c06e9
patrickluvsoj <patricklovesoj@gmail.com>
2022-10-24 20:26:49 -0700

    commeting our proxy setupProxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,23 +1,23 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const proxy = {
-    target: 'http://localhost:5000',
-    // headers: {
-    //   "Connection": "keep-alive"
-    // },
-    // changeOrigin: true
-}
+// const proxy = {
+//     target: 'http://localhost:5000',
+//     // headers: {
+//     //   "Connection": "keep-alive"
+//     // },
+//     // changeOrigin: true
+// }
 
-// setup conditional to use different keys!
-if (process.env.NODE_ENV === 'production') {
-  console.log('by passing hpm');
-} else {
-  console.log('setting HPM')
-  module.exports = function(app) {
-    app.use(['/auth/google', '/api'],
-      createProxyMiddleware(proxy)
-    );
-  };
-}
+// // setup conditional to use different keys!
+// if (process.env.NODE_ENV === 'production') {
+//   console.log('by passing hpm');
+// } else {
+//   console.log('setting HPM')
+//   module.exports = function(app) {
+//     app.use(['/auth/google', '/api'],
+//       createProxyMiddleware(proxy)
+//     );
+//   };
+// }
 
 
